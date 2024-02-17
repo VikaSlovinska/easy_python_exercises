77a8b4bdf8fb8f91f613911a4fd9ad4a379095d4
Viktoroya Slovinska <viktoriya@Viktoroyas-MBP.home>
2024-02-17 12:35:53 -0500

    New file

---
+++ total_calculation.py
@@ -0,0 +1,7 @@
+
+# calculate the total cost if user was to buy both 2bd and 3bd houses
+
+houses_prices = {'2bd': 500000, '3bd': 700000}
+total = houses_prices['2bd'] + houses_prices['3bd']
+
+print(f"The total will be {total} if you decide to buy both houses")
